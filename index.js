add723da1c6515963f0337209a2bb358cde7449c
Praise Folorunso <folorunsopraise580@gmail.com>
2024-04-13 22:53:23 +0100

    User registration, loging and email verification successfuly completed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv").config()
 const express = require( 'express' );
 const cors = require( 'cors' );
 const credentials = require( "./middleware/credentials" );
+const verifyJwt = require( "./middleware/verifyJWT" )
+const cookieParser = require('cookie-parser')
 
 const PORT = process.env.PORT || 3500
 const app = express();
@@ -12,10 +14,16 @@ app.use(credentials)
 app.use( cors() );
 app.use( express.json() )
 app.use( express.urlencoded( { extended: false } ) )
+app.use( cookieParser() )
 
 
 app.use( '/waitlist', require( './routes/waitlist' ) )
-app.use('/verify-mail', require('./routes/verify'))
+app.use('/auth', require("./routes/auth"))
+app.use( '/verify-mail', require( './routes/verify' ) )
+
+
+// Routes which requires authorization
+app.use(verifyJwt)
 
 
 
